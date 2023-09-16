510a2c1bc290b7d318fb6874aac389b3a1fbc467
server-imp <66805612+server-imp@users.noreply.github.com>
2023-09-16 06:39:50 +0200

    Rework codebase a little

--- memory.h
+++ memory.h
@@ -59,40 +59,77 @@ namespace memory
         }
     };
 
-    bool patch(memory::handle address, std::vector<uint8_t> buffer)
+    /// <summary>
+    /// Gets the current module filename
+    /// </summary>
+    std::string getCurrentModuleFileName()
+    {
+        char buffer[256]{};
+        if (!GetModuleBaseNameA(GetCurrentProcess(), GetModuleHandle(0), buffer, 256))
+            return "?";
+
+        return std::string(buffer);
+    }
+
+    /// <summary>
+    /// Writes a patch in a memory location
+    /// </summary>
+    /// <param name="address"></param>
+    /// <param name="buffer"></param>
+    /// <param name="originalBuffer">If this is not null, it will copy the original bytes here</param>
+    /// <returns>True if succeeded</returns>
+    bool patch(memory::handle address, std::vector<uint8_t> buffer, std::vector<uint8_t>* originalBuffer = nullptr)
     {
         if (!address.raw() || buffer.empty())
             return false;
 
+        auto ptr = address.as<uint8_t*>();
+
+        // Change the protection of the memory region to allow writing
         DWORD dwOldProtection{};
-        if (!VirtualProtect(address.raw(), buffer.size(), PAGE_EXECUTE_READWRITE, &dwOldProtection))
+        if (!VirtualProtect(ptr, buffer.size(), PAGE_EXECUTE_READWRITE, &dwOldProtection))
             return false;
 
-        if (memcpy_s(address.raw(), buffer.size(), buffer.data(), buffer.size()))
+        // If originalBuffer is not null, we copy the original bytes before overwriting them
+        if (originalBuffer)
+            originalBuffer->assign(ptr, ptr + buffer.size());
+
+        // Write the new bytes to memory
+        if (memcpy_s(ptr, buffer.size(), buffer.data(), buffer.size()))
         {
-            VirtualProtect(address.raw(), buffer.size(), dwOldProtection, &dwOldProtection);
+            // If the memcpy failed then we restore the original memory protection and return false
+            VirtualProtect(ptr, buffer.size(), dwOldProtection, &dwOldProtection);
 
             return false;
         }
 
+        // Flush the instruction cache to make sure it's not executing old instructions
         if (!FlushInstructionCache(GetCurrentProcess(), address.raw(), buffer.size()))
-        {
-            VirtualProtect(address.raw(), buffer.size(), dwOldProtection, &dwOldProtection);
+        { 
+            VirtualProtect(ptr, buffer.size(), dwOldProtection, &dwOldProtection);
 
             return false;
         }
 
-        VirtualProtect(address.raw(), buffer.size(), dwOldProtection, &dwOldProtection);
+        // Restore the original memory protection
+        VirtualProtect(ptr, buffer.size(), dwOldProtection, &dwOldProtection);
 
         return true;
     }
 
     namespace pattern
     {
+        /// <summary>
+        /// Parses an IDA-style signature e.g "DE AD ? ? BE EF"
+        /// </summary>
+        /// <returns>A pair of vectors with the pattern and its mask</returns>
         std::pair<std::vector<uint8_t>, std::vector<uint8_t>> parse_ida(const char* pattern)
         {
             std::vector<uint8_t> bytes{};
             std::vector<uint8_t> mask{};
+
+            if (!pattern)
+                return { {},{} };
 
             auto end = pattern + strlen(pattern);
             for (char* i = (char*)pattern; i < end; i++)
@@ -122,12 +159,19 @@ namespace memory
             return { bytes, mask };
         }
 
-        bool find(const char* module, const char* signature, memory::handle& result)
+        /// <summary>
+        /// Searches a module for the occurence of a byte pattern
+        /// </summary>
+        /// <param name="pattern">Pattern to search for</param>
+        /// <param name="result">A pointer to a memory handle to store the result in</param>
+        /// <param name="moduleName">The name of the module to search within, if null then it searches the main module</param>
+        /// <returns>True if pattern found</returns>
+        bool find(const char* pattern, memory::handle* result = nullptr, const char* moduleName = nullptr)
         {
-            if (!signature)
+            if (!pattern)
                 return false;
 
-            auto hModule = GetModuleHandleA(module);
+            auto hModule = GetModuleHandleA(moduleName);
             if (!hModule)
                 return false;
 
@@ -138,20 +182,20 @@ namespace memory
             auto start = reinterpret_cast<uint8_t*>(modInfo.lpBaseOfDll);
             auto end = start + modInfo.SizeOfImage;
 
-            auto parsed = parse_ida(signature);
-            auto pattern = parsed.first;
+            auto parsed = parse_ida(pattern);
+            auto bytes = parsed.first;
             auto mask = parsed.second;
 
-            for (size_t i = 0; i < modInfo.SizeOfImage - pattern.size(); i++)
+            for (size_t i = 0; i < modInfo.SizeOfImage - bytes.size(); i++)
             {
                 bool found = true;
 
-                for (size_t j = 0; j < pattern.size(); j++)
+                for (size_t j = 0; j < bytes.size(); j++)
                 {
                     if (!mask[j])
                         continue;
 
-                    if (*(start + i + j) != pattern[j])
+                    if (*(start + i + j) != bytes[j])
                     {
                         found = false;
                         break;
@@ -160,7 +204,59 @@ namespace memory
 
                 if (found)
                 {
-                    result = memory::handle(start + i);
+                    if (result)
+                        *result = memory::handle(start + i);
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Searches a module for the occurence of a string
+        /// </summary>
+        /// <param name="string">String to search for</param>
+        /// <param name="result">A pointer to a memory handle to store the result in</param>
+        /// <param name="moduleName">The name of the module to search within, if null then it searches the main module</param>
+        /// <returns>True if string found</returns>
+        bool find_string(const char* string, memory::handle* result = nullptr, const char* moduleName = nullptr)
+        {
+            if (!string)
+                return false;
+
+            auto strLen = strlen(string);
+
+            auto hModule = GetModuleHandleA(moduleName);
+            if (!hModule)
+                return false;
+
+            MODULEINFO modInfo{};
+            if (!GetModuleInformation(GetCurrentProcess(), hModule, &modInfo, sizeof(MODULEINFO)))
+                return false;
+
+            auto start = reinterpret_cast<uint8_t*>(modInfo.lpBaseOfDll);
+            auto end = start + modInfo.SizeOfImage;
+
+            for (size_t i = 0; i < modInfo.SizeOfImage; i++)
+            {
+                bool found = true;
+
+                for (size_t j = 0; j < strLen; j++)
+                {
+
+                    if (*(start + i + j) != string[j])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    if (result)
+                        *result = memory::handle(start + i);
 
                     return true;
                 }
@@ -169,6 +265,77 @@ namespace memory
             return false;
         }
     }
+
+    /// <summary>
+    /// Helper class to find patterns and apply patches on them
+    /// </summary>
+    class Patch
+    {
+    private:
+        const char* name{};
+        memory::handle pointer{};
+
+        std::vector<uint8_t> buffer{};
+        std::vector<uint8_t> originalBuffer{};
+
+        bool valid{};
+        bool enabled{};
+
+    public:
+        Patch(const char* name, std::vector<uint8_t> buffer, const char* pattern, const char* moduleName = nullptr)
+        {
+            this->name = name;
+            this->buffer = buffer;
+
+            if (!pattern::find(pattern, &pointer, moduleName))
+            {
+                log("Couldn't Find \"%s\"!", name);
+
+                return;
+            }
+
+            log("Found \"%s\" at %s+%08X", name, moduleName ? moduleName : getCurrentModuleFileName().c_str(), pointer.as<uint8_t*>() - (uint8_t*)GetModuleHandle(0));
+            valid = true;
+        }
+
+        const bool is_valid() const
+        {
+            return valid;
+        }
+
+        const bool is_enabled() const
+        {
+            return enabled;
+        }
+
+        bool enable()
+        {
+            if (enabled || !valid || !memory::patch(pointer, buffer, &originalBuffer))
+            {
+                log("Couldn't Patch \"%s\"!", name);
+                return false;
+            }
+
+            log("Patched \"%s\"!", name);
+
+            enabled ^= true;
+            return true;
+        }
+
+        bool disable()
+        {
+            if (!enabled || !valid || !memory::patch(pointer, originalBuffer))
+            {
+                log("Couldn't Unpatch \"%s\"!", name);
+                return false;
+            }
+
+            log("Unpatched \"%s\"!", name);
+
+            enabled ^= true;
+            return true;
+        }
+    };
 }
 
 #endif // !MEMORY
--- pch.h
+++ pch.h
@@ -4,14 +4,30 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <string>
 #include <cstdint>
 #include <thread>
 #include <filesystem>
 #include <psapi.h>
 #include <vector>
 #include <stdio.h>
+#include <format>
 #pragma comment(lib, "user32.lib")
 
 using namespace std::chrono_literals;
+
+inline void log(const char* fmt, ...)
+{
+    printf(std::format("[SFAE] {:%T}\t", std::chrono::floor<std::chrono::seconds>(std::chrono::system_clock::now())).c_str());
+
+    va_list args;
+    va_start(args, fmt);
+
+    vprintf(fmt, args);
+
+    va_end(args);
+
+    printf("\n");
+}
 
 #endif
