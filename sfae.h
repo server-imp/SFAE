2b122f97f01dbf92b452779c88eb4633b8e49f0b
server-imp <66805612+server-imp@users.noreply.github.com>
2025-06-05 07:58:19 +0200

    Bump version

--- sfae.h
+++ sfae.h
@@ -10,7 +10,7 @@
 #include "memory.h"
 #include "hook.h"
 
-#define SFAE_VERSION "2.0.0"
+#define SFAE_VERSION "2.0.1"
 
 const char* MESSAGE = "SFAE Version " SFAE_VERSION ":\nAchievements are enabled!";
 const wchar_t* WIDE_MESSAGE = L"SFAE Version " SFAE_VERSION L":\nAchievements are enabled!";
